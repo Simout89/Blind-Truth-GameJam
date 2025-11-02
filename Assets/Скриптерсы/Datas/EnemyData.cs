2b10a0912f020cd1bc544b72e8c620049ae1126b
Albert <alik2006sootkill@gmail.com>
2025-11-02 13:12:03 +0700

    Implements basic damage system
    Adds basic damage system with damageable interface, body parts, and corresponding data assets.
    
    The player and enemy now have health components that can take damage.
    
    A BodyPart script was added to allow for different damage multipliers based on where the entity is hit.
    A new EnemyData ScriptableObject was added for enemy health configuration.

---
+++ Assets/Скриптерсы/Datas/EnemyData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Скриптерсы.Datas
+{
+    [CreateAssetMenu(menuName = "Datas/EnemyData")]
+    public class EnemyData: ScriptableObject
+    {
+        [field: SerializeField] public float Health { get; private set; } = 3f;
+    }
+}
---
+++ Assets/Скриптерсы/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace Скриптерсы
+{
+    public class PlayerHealth: MonoBehaviour, IDamageable
+    {
+        [SerializeField] private CharacterController _characterController;
+        private float currentHealth;
+
+        private void Awake()
+        {
+            currentHealth = _characterController.CharacterControllerData.Health;
+        }
+
+        public void TakeDamage(DamageInfo damageInfo)
+        {
+            currentHealth = Mathf.Max(currentHealth - damageInfo.Count, 0);
+
+            if (currentHealth <= 0)
+            {
+                Debug.Log("Смерть");
+            }
+        }
+    }
+
+    public interface IDamageable
+    {
+        public void TakeDamage(DamageInfo damageInfo);
+    }
+    
+    public struct DamageInfo
+    {
+        public float Count;
+        public readonly string DamageDealerName;
+
+        public DamageInfo(float count, string damageDealerName)
+        {
+            Count = count;
+            DamageDealerName = damageDealerName;
+        }
+    }
+}
