727d91a6cc2457e90bfca482aa2be8fd1795cefa
Albert <alik2006sootkill@gmail.com>
2025-11-02 15:32:41 +0700

    баг фикс

--- Assets/Скриптерсы/PlayerInteraction.cs
+++ Assets/Скриптерсы/PlayerInteraction.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,7 +13,8 @@ namespace Скриптерсы
         public event Action OnClickableEnter;
         public event Action OnClickableExit;
 
-        private IClickable _clickable;
+        private List<IClickable> _clickables = new List<IClickable>();
+        private List<IHighlightable> _highlightables = new List<IHighlightable>();
 
         private bool enable = true;
         
@@ -31,12 +34,62 @@ namespace Скриптерсы
         {
             if(!enable) return;
 
-            if (_clickable != null)
-            {
-                var info = _clickable.Click();
+            CleanUpDestroyedObjects();
 
-                if (info.hideClickableView) OnClickableExit?.Invoke();
+            if (_clickables.Count > 0)
+            {
+                var closestClickable = GetClosestClickable();
+                if (closestClickable != null)
+                {
+                    var info = closestClickable.Click();
+
+                    if (info.hideClickableView)
+                    {
+                        OnClickableExit?.Invoke();
+                        
+                        // Проверяем, остались ли еще объекты после взаимодействия
+                        CleanUpDestroyedObjects();
+                        if (_clickables.Count > 0 || _highlightables.Count > 0)
+                        {
+                            OnClickableEnter?.Invoke();
+                        }
+                    }
+                }
             }
+        }
+
+        private void CleanUpDestroyedObjects()
+        {
+            _clickables.RemoveAll(c => c == null || (c is MonoBehaviour mb && mb == null));
+            _highlightables.RemoveAll(h => h == null || (h is MonoBehaviour mb && mb == null));
+        }
+
+        private IClickable GetClosestClickable()
+        {
+            if (_clickables.Count == 0) return null;
+            
+            CleanUpDestroyedObjects();
+            
+            if (_clickables.Count == 0) return null;
+            if (_clickables.Count == 1) return _clickables[0];
+
+            IClickable closest = null;
+            float minDistance = float.MaxValue;
+
+            foreach (var clickable in _clickables)
+            {
+                if (clickable is MonoBehaviour mb && mb != null)
+                {
+                    float distance = Vector3.Distance(transform.position, mb.transform.position);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closest = clickable;
+                    }
+                }
+            }
+
+            return closest;
         }
 
         private void HandleCanceled(InputAction.CallbackContext obj)
@@ -48,12 +101,25 @@ namespace Скриптерсы
         {
             if (other.gameObject.TryGetComponent<IHighlightable>(out var h))
             {
-                h.Highlight();
-                OnClickableEnter?.Invoke();
+                if (!_highlightables.Contains(h))
+                {
+                    _highlightables.Add(h);
+                    h.Highlight();
+                    
+                    if (_highlightables.Count == 1)
+                    {
+                        OnClickableEnter?.Invoke();
+                    }
+                }
             }
 
-            if (other.gameObject.TryGetComponent<IClickable>(out var c)) _clickable = c;
-            
+            if (other.gameObject.TryGetComponent<IClickable>(out var c))
+            {
+                if (!_clickables.Contains(c))
+                {
+                    _clickables.Add(c);
+                }
+            }
             
             Debug.Log(other.name);
         }
@@ -62,11 +128,22 @@ namespace Скриптерсы
         {
             if (other.gameObject.TryGetComponent<IHighlightable>(out var h))
             {
-                h.RemoveHighlight(); 
-                OnClickableExit?.Invoke();
+                if (_highlightables.Contains(h))
+                {
+                    _highlightables.Remove(h);
+                    h.RemoveHighlight();
+                    
+                    if (_highlightables.Count == 0)
+                    {
+                        OnClickableExit?.Invoke();
+                    }
+                }
             }
             
-            if (other.gameObject.TryGetComponent<IClickable>(out var c)) _clickable = null;
+            if (other.gameObject.TryGetComponent<IClickable>(out var c))
+            {
+                _clickables.Remove(c);
+            }
         }
 
         public void Enable()
