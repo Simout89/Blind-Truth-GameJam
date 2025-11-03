855431eb7c4a10170199a0afe9da6d0c01eecf62
Mykola Baranov <110630608+herynax@users.noreply.github.com>
2025-11-03 20:16:28 +0100

    -Play Oneshot

--- Assets/Скриптерсы/Sound/PlayOneShot.cs
+++ Assets/Скриптерсы/Sound/PlayOneShot.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using FMODUnity;
 
 public class PlayOneShot : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private EventReference soundEventReference;
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySoundEvent()
     {
-        
+        if (soundEventReference.IsNull == false)
+        {
+            RuntimeManager.PlayOneShot(soundEventReference);
+        }
     }
 }
