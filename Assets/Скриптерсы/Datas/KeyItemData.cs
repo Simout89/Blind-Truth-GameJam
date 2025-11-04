fa80d62bd372d68224d1f92f51ab1c700a848903
Albert <alik2006sootkill@gmail.com>
2025-11-04 20:31:40 +0700

    wwww

--- Assets/Скриптерсы/Datas/KeyItemData.cs
+++ Assets/Скриптерсы/Datas/KeyItemData.cs
@@ -7,5 +7,6 @@ namespace Скриптерсы.Datas
     {
         [field: SerializeField] public string Name { get; private set; } = "www";
         [field: SerializeField] public string PickUpSound { get; private set; }
+        [field: SerializeField] public string textOnSubtitles { get; private set; }
     }
 }
--- Assets/Скриптерсы/Interactable/KeyItem.cs
+++ Assets/Скриптерсы/Interactable/KeyItem.cs
@@ -2,12 +2,16 @@
 using UnityEngine;
 using Zenject;
 using Скриптерсы.Datas;
+using Скриптерсы.View;
 
 namespace Скриптерсы.Interactable
 {
     public class KeyItem: HighlightableObject, IClickable
     {
         [SerializeField] private KeyItemData _keyItemData;
+
+
+        [Inject] private SubtitlesView _subtitlesView;
         [Inject] private PlayerInteraction _playerInteraction;
         public ClickResult Click()
         {
@@ -15,6 +19,9 @@ namespace Скриптерсы.Interactable
             
             if(!string.IsNullOrEmpty(_keyItemData.PickUpSound))
                 RuntimeManager.PlayOneShot(_keyItemData.PickUpSound, transform.position);
+            
+            if(!string.IsNullOrEmpty(_keyItemData.textOnSubtitles))
+                _subtitlesView.ShowText(_keyItemData.textOnSubtitles);
             
             Destroy(gameObject);
             
